08f268ce69249d39c7edb51f85ffa5fdc5bc1a03
Eric Lautanen <ericlautanen77@gmail.com>
2026-02-19 21:29:33 -0500

    force mingw64 to use static files...

--- ffmpeg-sys-the-third/build.rs
+++ ffmpeg-sys-the-third/build.rs
@@ -866,8 +866,8 @@ fn link_to_libraries(statik: bool) {
     }
     if statik {
         println!("cargo:rustc-link-search=native=/mingw64/lib");
-        println!("cargo:rustc-link-arg=/mingw64/lib/libx264.a");
-        println!("cargo:rustc-link-arg=/mingw64/lib/libz.a");
+        println!("cargo:rustc-link-lib=x264");
+        println!("cargo:rustc-link-lib=z");
         println!("cargo:rustc-link-lib=bcrypt");
         println!("cargo:rustc-link-lib=ole32");
         println!("cargo:rustc-link-lib=user32");
